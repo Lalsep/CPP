1ca201afb722bf1172fda65389de25ee7ec9388c
Wojciech Balcerzak <wojciech.balcerzak@orpeg.pl>
2019-01-19 09:05:29 +0100

    zmiana mnozenie

--- moje_cwiczenia/mnozenie liczb.cpp
+++ moje_cwiczenia/mnozenie liczb.cpp
@@ -10,18 +10,17 @@ int main ()
     std::cout << std::endl;
     std::cout << "podaj druga liczbe  y = ";
     std::cin >> y;
-    std::cout << std::endl;
     wynik = x*y;
+    std::cout << std::endl;
     std::cout << "wynik = " << wynik << std::endl;
-    
+    std::cout << std::endl;
     wynik*=2;
-    
-    std::cout << "wynik = " << wynik << std::endl;
+    std::cout << "podwojny wynik = " << wynik << std::endl;
     int result;
     result = 2*wynik;
-    std::cout << "drugi wynik = " << result << std::endl;
-    
-    
+    std::cout << std::endl;
+    std::cout << "poczworny wynik = " << result << std::endl;
+    std::cout << std::endl;
     
     return 0;
 }
