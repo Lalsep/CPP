6bd144b6032cc38859e11f554f9cee0560bfe5b4
Wojciech Balcerzak <wbalcerz@gmail.com>
2019-01-26 10:58:21 +0100

    pliki

---
+++ moje_cwiczenia/choinka.cpp
@@ -0,0 +1,18 @@
+// Napisz program, który wypisuje na ekranie choinkę za pomocą znaków "*".
+// W pierwszej linii na górze powinien być jeden "*".
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "     *\n" << std::endl;
+    std::cout << "     *\n";
+    std::cout << "    ***\n";
+    std::cout << "   *****\n";
+    std::cout << "  *******\n";
+    std::cout << " *********\n";
+    std::cout << "************\n";
+    
+    
+    return 0;
+}
---
+++ moje_cwiczenia/powitanie.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main ()
+{
+    std::cout << std::endl;
+    std::cout << "Dzień dobry \n";
+    std::cout << "\n";
+    
+    
+    
+    return 0;
+}
