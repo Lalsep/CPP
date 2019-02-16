05c8d6a750458a6cc9076a6b10048bcd6f85dac7
Wojciech Balcerzak <wbalcerz@gmail.com>
2019-02-16 07:15:06 +0100

    nowe pliki cpp

---
+++ double.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+
+double x;
+x = 10.0/3;
+std::cout << "liczba double x = " << x << std::endl;
+return 0;
+
+
+
+}
+
+
---
+++ znak_specjalny.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Moją ulubioną powieścią jest \"Potop\""<<endl;
+    
+    
+    
+    return 0;
+}
