31ec6f8b0e7b677b149fab026414f336d06e6610
Wojciech Balcerzak <wbalcerz@gmail.com>
2019-04-28 16:35:18 +0200

    program cpp pierwszy

--- pierwszy.cpp
+++ pierwszy.cpp
@@ -3,7 +3,9 @@
 int main()
 {
     
-    std::cout << "to jest wydruk \n";
+    std::cout << "1. to jest wydruk \n";
+    std::cout<<"2. to jest druga linika" <<std::endl;
+    std::cout << "3. \n";
     
     return 0;
 }
